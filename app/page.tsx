f6c6ac1f08dd533a251ed7a5c913e22e9a724a44
convexproject24 <convexproject@gmail.com>
2025-02-08 22:33:19 +0900

    initial set up of project

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,14 @@ export default function Home() {
   return (
 
   <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <SignedIn>
+<SignedIn>
       <SignOutButton>
+        <Button>Sign Out</Button>
+      </SignOutButton>
+    </SignedIn>
+    <SignedOut>
+      <SignInButton mode="modal">
+          <Button>Sign In</Button>
       </SignInButton>
     </SignedOut>
 
